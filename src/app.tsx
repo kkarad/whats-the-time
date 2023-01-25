e15c3c30f7f571dac6a0699acc4942f09a90a631
Konstantinos Karadamoglou <kkarad@gmail.com>
2023-01-25 23:42:45 +0000

    design time stack

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,18 @@
-
 export function App() {
-  return (
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
-  )
+    return (
+        <>
+            <div class="px-60 py-4">
+                <div className="flex flex-col items-center">
+                    <div className="mt-2 bg-slate-400">
+                        <span>HK</span>
+                        <span>11:11:11</span>
+                    </div>
+                    <div className="my-2 bg-slate-400">
+                        <span>LDN</span>
+                        <span>12:12:12</span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
